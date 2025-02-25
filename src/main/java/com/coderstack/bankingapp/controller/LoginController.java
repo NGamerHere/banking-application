c98cda0b5a72acb282015cc02c06d6c07a5099e2
NGamerHere <codingdatta@gmail.com>
2025-02-25 22:37:52 +0530

    added the jwt token

--- src/main/java/com/coderstack/bankingapp/controller/LoginController.java
+++ src/main/java/com/coderstack/bankingapp/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.coderstack.bankingapp.controller;
 import com.coderstack.bankingapp.Entity.Login;
 import com.coderstack.bankingapp.Entity.User;
 import com.coderstack.bankingapp.repository.UserRepository;
+import com.coderstack.bankingapp.security.TokenManager;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,8 +39,15 @@ public class LoginController {
 
         Optional<User> n=userRepository.findByEmail(login.email);
        if(n.isPresent()) {
-           System.out.println(login.password);
            if(n.get().verifyPassword(login.password)){
+
+            Map<String, Object> tokenData=new HashMap<>();
+            Map.put("user_id",n.get().getId());
+
+               TokenManager newToken=new TokenManager();
+
+               response.put("token", newToken.generateToken(response));
+               
                return new ResponseEntity<>(response, HttpStatus.OK);
            }else{
             response.put("error", "invalid username     and password");
